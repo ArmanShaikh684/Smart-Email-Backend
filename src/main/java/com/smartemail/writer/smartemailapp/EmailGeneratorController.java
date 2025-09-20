ae2013ff44a042a8019cdfd550231ed05341d37c
ArmanShaikh684 <armanshaikh.as684@gmail.com>
2025-09-20 19:30:44 +0530

    Initial commit: Add email writer backend project

---
+++ src/main/java/com/smartemail/writer/smartemailapp/EmailGeneratorController.java
@@ -0,0 +1,20 @@
+package com.smartemail.writer.smartemailapp;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email/generate")
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/smartemail/writer/smartemailapp/EmailRequest.java
@@ -0,0 +1,10 @@
+package com.smartemail.writer.smartemailapp;
+
+import lombok.Data;
+
+@Data
+public class EmailRequest {
+    private String emailContent;
+    private String tone;
+
+}
